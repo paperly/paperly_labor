86a780ded3615d764135928145d8f0bd13c39d82
mariokeller <mariokeller@marios-imac.speedport_w_921v_1_22_000>
2013-09-28 17:33:05 +0200

    fiffi

--- php/timeline.php
+++ php/timeline.php
@@ -6,7 +6,7 @@ include "php/functions.php";
 include "php/functionsTimeline.php";
 $user_id = $_SESSION["user_id"];
 // global values
-// wisnversion 1.0 jan
+// wisnversion 1.0 fiffi
 $target = '/';
 
 ?>
