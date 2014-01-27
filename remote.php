ad2477689fdafc86b08c795f91e43f16bde4f204
mariokeller <mariokeller@marios-imac.speedport_w_921v_1_24_000>
2014-01-27 02:56:13 +0100

    modal article Ansicht tauschen

--- remote.php
+++ remote.php
@@ -1,3 +1,30 @@
-<?php
+
+
+ <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <?php
+$var = $_GET["var"];
+echo $var;
 
 ?>
+          
+         <button onclick="load_article('halldfsowelt');">
+  hallo1
+</button> 
+          
+          
+          
+      </div><article class="timeline-article" style="width: 100%; margin-left: -2px; padding: 0px;"><div id="adsense">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- start big -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:970px;height:90px"
+     data-ad-client="ca-pub-7791364251025556"
+     data-ad-slot="6688671025"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script></div> </article>   
+      <div class="modal-footer">
--- test2.php
+++ test2.php
@@ -7,85 +7,82 @@
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link href="css/bootstrap.min.css" rel="stylesheet">
-         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-   <!-- <script src="https://code.jquery.com/jquery.js"></script> -->
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <!-- jquery -->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <!-- <script src="https://code.jquery.com/jquery.js"></script> -->
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <!-- jquery -->
         <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
         <script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
-    <script src="js/libs/bootstrap/bootstrap.min.js"></script>
-    
-     
-       
-       
-    <script>
-        
-        
-        
-        function load_article(){
-            
-            
-         
-         //   $('#test').load('remote.html');
-                    //   $('#test'). html("less...");
-            
-           // alert("html");
-   //  $('.modal-content').load('remote.html');
-  
-      
+        <script src="js/libs/bootstrap/bootstrap.min.js"></script>
 
-  
- //      $('#lala').html("less...");
- $('#lala').load("remote.html");
-            
-    $('#myModal').modal('show');
-            
-    //$.post("php/unlike_add.php", { user: user, article: article });
-    //$('#likebox_'+article+'').load('php/load_likebox.php?article_id='+article+'');
-		
-}
 
-    </script>
-    
-    
+
+
+        <script>
+
+
+
+function load_new_article(id) {
+              //  $('#myModal').modal('hide');
+
+                $('#lala').load("remote.php?var=" + id);
+               
+                $('#myModal').modal('show');
+
+            }
+
+
+            function load_article(id) {
+
+                $('#lala').load("remote.php?var=" + id);
+                $('#myModal').modal('show');
+
+            }
+
+        </script>
+
+
     </head>
     <body>
         <div>TODO write content</div>
-        
-        
+
+
         <!-- Button trigger modal -->
-<button onclick="load_article();">
-  hallo
-</button>
-        
-        <div id="test">1</div>
-        
-        
-        
-        
+        <button onclick="load_article('3dgdgiodgfjdgdgfgdgsdg');">
+            hallo1
+        </button>
+        <button onclick="load_article('dgdfgdfg');">
+            hallo2
+        </button>     
 
- <a data-toggle="modal" data-target="#myModal" href="remote.html"  >Seite 2</a>
 
- 
- 
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-         <div id="lala">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div></div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+
+
+
+
+
+        <a data-toggle="modal" data-target="#myModal" href="remote.html"  >Seite 2</a>
+
+
+
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div id="lala">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div></div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
 
 
     </body>
