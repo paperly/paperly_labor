ca0f87f324955f98cc7a51825acdc6ecab9904e8
mariokeller <mariokeller@marios-imac.speedport_w_921v_1_24_000>
2014-01-27 19:07:22 +0100

    modal

--- remote.php
+++ remote.php
@@ -1,30 +1,57 @@
 
 
- <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        <?php
-$var = $_GET["var"];
-echo $var;
-
-?>
-          
-         <button onclick="load_article('halldfsowelt');">
-  hallo1
-</button> 
-          
-          
-          
-      </div><article class="timeline-article" style="width: 100%; margin-left: -2px; padding: 0px;"><div id="adsense">
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- start big -->
+<div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+    <h4 class="modal-title" id="myModalLabel">Alinaz Arena baut Mobilfunk Versorgung aus</h4>
+</div>
+<div class="modal-body">
+   
+    <div class="modal-container">
+    <div class="modal-ad-left">
+       <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Article banner left -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:970px;height:90px"
+     style="display:inline-block;width:160px;height:600px"
      data-ad-client="ca-pub-7791364251025556"
-     data-ad-slot="6688671025"></ins>
+     data-ad-slot="4326327020"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
-</script></div> </article>   
-      <div class="modal-footer">
+</script>
+    </div>
+    <div class="article-content">
+
+        <img  src="http://paperly.de/upload/article/paperly_article_183459.jpg">
+
+
+        <div class="article-content-text">
+            
+            
+            <p>Die Allianz Arena baut mit Zusammenarbeit der Telekom die Mobilfunkversorgung im Stadion&nbsp;drastisch aus. <br><br>Während einem Fußballspiel der Bayern Profis sollen problemlos über 50.000 Zuschauer auf ein lokales WLAN-Netzwerk zugreifen können, was weltweit einzigartig wäre in einem Fußballstadion.&nbsp;<br><br>Zudem sollen&nbsp;die Bayern-Fans bei den Heimspielen ihres Klubs über die neue Mobilfunk-Technologie LTE mit Höchstgeschwindigkeit im Netz surfen. Der Münchner Fußball-Tempel ist das erste Stadion in Deutschland, dass die Telekom mit dem neuen Highspeed-Standard versorgt. Die Mobilfunkversorgung mit den Standards GSM und UMTS wird zudem ebenfalls&nbsp;aufgestockt.<br><br>Damit ist es nun kein Problem mehr&nbsp;während des Fußballspiels Videos zu streamen, Bilder zu teilen und Spielstände in anderen Stadien nachsehen zu können.&nbsp;<br><br><span>Nebenbei wollen der FC Bayern München und die Telekom mit "StadiumVision" die Münchner Arena zu einem multimedialen Erlebnisraum für alle Fußball-Fans machen. Die Kommunikation soll ab Mitte 2014&nbsp;die Arena schrittweise für eine neuartige Form der Kommunikation mit den Fans vor, während und nach den Spielen sorgen. <br><br>Zusätzliche Informationen zum Spiel, Live-Streams, Video- Wiedergaben oder Szenen aus früheren Partien sowie Unterhaltungsangebote werden auf zusätzlichen Bildschirmen überall im Stadion und auf dem eigenen Smartphone zu empfangen sein.</span><br><br><br></p>
+        </br></br></br>
+  
+        </div>
+ 
+
+        
+
+
+    </div>
+    <div class="modal-ad-boottom">
+        
+        
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- article banner bottom -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:468px;height:60px"
+     data-ad-client="ca-pub-7791364251025556"
+     data-ad-slot="8896127420"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script> 
+    </div>
+    </div>
+
+
+    </div>
+
+</div>
--- test2.php
+++ test2.php
@@ -61,25 +61,14 @@ function load_new_article(id) {
 
 
 
-        <a data-toggle="modal" data-target="#myModal" href="remote.html"  >Seite 2</a>
-
+       
 
 
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div id="lala">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
                         </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div></div>
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
         </div><!-- /.modal -->
